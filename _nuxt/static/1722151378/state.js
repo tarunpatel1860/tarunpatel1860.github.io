c91dc63e2af4a5d14dad8c0d37e7f9d9edc5d832
Tarun Patel <tarunpatel1860@gmail.com>
2024-07-28 12:53:20 +0530

    Updates

---
+++ _nuxt/static/1722151378/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftarunpatel1860.github.io\u002F_nuxt\u002Fstatic\u002F1722151378",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftarunpatel1860.github.io\u002F",assetsPath:"\u002Ftarunpatel1860.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
